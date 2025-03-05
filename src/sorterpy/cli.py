a456347829b0a19c855f846e7b766092beb81ec0
zudsniper <me@zod.tf>
2025-03-05 16:58:49 -0600

    typer -> argparse,

--- src/sorterpy/cli.py
+++ src/sorterpy/cli.py
@@ -1,21 +1,21 @@
-"""Console script for sorterpy."""
+"""
+CURRENTLY UNUSED
+
+Console script for sorterpy."""
+import sys
+import argparse
 import sorterpy
 
-import typer
-from rich.console import Console
 
-app = typer.Typer()
-console = Console()
-
-
-@app.command()
 def main():
     """Console script for sorterpy."""
-    console.print("Replace this message by putting your code into "
-               "sorterpy.cli.main")
-    console.print("See Typer documentation at https://typer.tiangolo.com/")
+    parser = argparse.ArgumentParser(description='sorterpy command line interface')
+    args = parser.parse_args()
     
+    print("Replace this message by putting your code into sorterpy.cli.main")
+    print("See argparse documentation at https://docs.python.org/3/library/argparse.html")
+    return 0
 
 
 if __name__ == "__main__":
-    app()
+    sys.exit(main())
