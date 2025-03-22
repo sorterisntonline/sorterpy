1a06164ecfd01be13e34b107d71beed5f6c68c1e
zod <me@zod.tf>
2025-03-21 20:01:37 -0500

    make tests pass before we have tests

--- tests/test_sorterpy.py
+++ tests/test_sorterpy.py
@@ -16,9 +16,11 @@ def response():
     """
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    return True
 
 
 def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    assert True
